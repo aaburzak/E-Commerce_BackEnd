aef130aa6343473b7693a4bcedeed00ee60a57bb
Aaron Burzak <aaron.j.burzak@gmail.com>
2022-03-25 11:27:13 -0400

    built out product-routes.js

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -7,12 +7,42 @@ const { Product, Category, Tag, ProductTag } = require('../../models');
 router.get('/', (req, res) => {
   // find all products
   // be sure to include its associated Category and Tag data
+  Product.findAll({
+    include: [
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+  .then(dbCategory => res.json(dbCategory))
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 // get one product
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [
+      Category,
+      {
+        model: Tag,
+        through: ProductTag
+      }
+    ]
+  })
+    .then(dbCategory => json(dbCategory))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+  });
 });
 
 // create new product
@@ -91,6 +121,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy({
+    where: {
+      id: req.params.id
+    }
+  })
+  .then(dbCategory => {
+    if (!dbCategory) {
+      res.status(404).json({ message: 'No products found with this id'});
+      return;
+    }
+    res.json(dbCategory);
+  })
+  .catch(err => {
+    console.log(err);
+    res.status(500).json(err);
+  });
 });
 
 module.exports = router;
